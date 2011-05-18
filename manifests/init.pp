c84a1ce967a2289e7618a3169c2852d8ad0db7a6
Pete de Zwart <dezwart@gmail.com>
2011-05-19 00:14:07 +1000

    Massaged recipie from http://projects.puppetlabs.com/projects/1/wiki/Resolv_Conf_Patterns

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+# UNIX Resolver
+#
+# == Parameters
+#
+# [*domain_name*]
+#  The default domain.
+#
+# [*search_path*]
+#   Array of domains to search.
+#
+# [*name_servers*]
+#   Array of nameservers to search.
+#
+# == Examples
+#
+#  class { 'resolv':
+#   domain_name		=> 'mydomain',
+#   search_path		=> [ 'mydomain', 'test.mydomain' ],
+#   name_servers	=> [ '10.0.0.1', '10.0.0.2' ],
+#  }
+#
+class resolv( $domain_name = undef,
+	$search_path = undef,
+	$name_servers = undef ) {
+	file { "/etc/resolv.conf":
+		owner   => root,
+		group   => root,
+		mode    => '0644',
+		content => template("resolv/resolv.conf.erb"),
+	}
+}
