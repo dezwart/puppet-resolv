38d7c1b5391e774a38f28cb4e8353288d93490dc
Pete de Zwart <dezwart@gmail.com>
2012-12-27 23:08:52 +1100

    Linted.

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,19 +13,22 @@
 #
 # == Examples
 #
-#  class { 'resolv':
-#   domain_name     => 'mydomain',
-#   search_path     => [ 'mydomain', 'test.mydomain' ],
-#   name_servers    => [ '10.0.0.1', '10.0.0.2' ],
-#  }
+# class { 'resolv':
+#   domain_name   => 'mydomain',
+#   search_path   => [ 'mydomain', 'test.mydomain' ],
+#   name_servers  => [ '10.0.0.1', '10.0.0.2' ],
+# }
 #
 class resolv( $domain_name = undef,
-    $search_path = undef,
-    $name_servers = undef ) {
-    file { "/etc/resolv.conf":
-        owner   => root,
-        group   => root,
-        mode    => '0644',
-        content => template("resolv/resolv.conf.erb"),
-    }
+  $search_path = undef,
+  $name_servers = undef ) {
+
+  file { '/etc/resolv.conf':
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('resolv/resolv.conf.erb'),
+  }
 }
+
+/* vim: set ts=2 sw=2 sts=2 tw=0 et:*/
